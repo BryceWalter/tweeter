9f0c755abff52c8b68965b8c3b5a09a2fbc656af
Bryce <brycewalterparker@gmail.com>
2017-09-19 17:35:29 +0000

    created hardcoded tweet container with hover functionality to show buttons

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,14 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function(){
+//Hover state on/off for tweets
+  $(".tweet").on('mouseenter', function(){
+    var buttons = $(".tweet > footer").children('div')
+    buttons.removeClass("hidden")
+  })
+  $(".tweet").on('mouseleave', function(){
+    var buttons = $(".tweet > footer").children('div')
+    buttons.addClass("hidden")
+  })
+})
