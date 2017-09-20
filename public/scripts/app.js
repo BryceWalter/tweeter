1231031f7c4d923cc2df340de10a8ffdb26507d4
Bryce <brycewalterparker@gmail.com>
2017-09-20 18:17:06 +0000

    created AJAX to update tweet database with user tweet input

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,52 +7,7 @@
 
 $(document).ready(function() {
 // Test / driver code (temporary). Eventually will get this from the server.
-  var tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-]
+// ########## FUNCTIONS
 
   function createTweetElement(tweet) {
     return $(`<article class="tweet">
@@ -62,7 +17,7 @@ $(document).ready(function() {
                     <h5>${tweet.user.handle}</h5>
                   </header>
 
-                  <div class="tweet-body"><p>${tweet.content.text}</p></div>
+                  <div class="tweet-body"><p></p></div>
 
                   <footer>
                     <p>${moment(tweet.created_at).fromNow()}</p>
@@ -75,15 +30,40 @@ $(document).ready(function() {
                 </article>`)
   }
 
-
   function renderTweets(tweets) {
     for (tweet of tweets) {
       let $tweet = createTweetElement(tweet)
       $('#tweet-log').prepend($tweet)
+      $('#tweet-log').find(".tweet-body p:first").text(tweet.content.text)
     }
   }
 
-  renderTweets(tweetData)
+  function loadTweets() {
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+      success: function (tweetData) {
+        renderTweets(tweetData)
+      }
+    })
+  }
+
+// ########## EVENT HANDLERS
+  var $form = $('.tweet-input')
+  $form.on('submit', function(event){
+    event.preventDefault()
+    var tweetInput = $form.serialize()
+    $.ajax({
+      url: "/tweets",
+      method: "POST",
+      data: tweetInput
+    })
+
+    loadTweets()
+  })
+
+
+
 
   // Test / driver code (temporary)
    // to see what it looks like
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('form > textarea').on('keyup', function(){
-    var chars = 140 - +$(this).val().length
-    var counterSpan = $(this).siblings('span')
+    var chars = 140 - $(this).val().length
+    var counterSpan = $('.counter')
     counterSpan.text(chars)
     if (chars < 0) {
       counterSpan.addClass("text-too-long")
